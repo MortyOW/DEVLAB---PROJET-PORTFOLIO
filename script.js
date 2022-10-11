65b3c5626ac0b914dff0bc7f10366deb712f4e24
MortyOW <adam.simon@edu.devinci.fr>
2022-10-11 15:49:02 +0200

    :sparkles: Page Compétence/Accueil

---
+++ script.js
@@ -0,0 +1,13 @@
+/* Changement du bouton clique */
+console.log('salut')
+
+function ChangeName() {
+console.log('salut')
+document.getElementById("changeMe").innerHTML = "géchangé";
+}
+
+
+function Refresh(){
+if (document.getElementById("changeMe").innerHTML = "géchangé" && document.getElementById('changeMe').clicked == true) {
+    document.location.reload();
+}}

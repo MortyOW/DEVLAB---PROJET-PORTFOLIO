a17bb7793112498d3eca0e5ee7d547a0fee63cb9
MortyOW <adam.simon@edu.devinci.fr>
2022-10-11 17:03:53 +0200

    UI page accueil

--- script.js
+++ script.js
@@ -2,12 +2,17 @@
 console.log('salut')
 
 function ChangeName() {
-console.log('salut')
-document.getElementById("changeMe").innerHTML = "géchangé";
+    console.log('salut')
+    if (document.getElementById("changeMe").innerHTML == "Clique") {
+        document.getElementById("changeMe").innerHTML = "Refresh"
+    } else if (document.getElementById("changeMe").innerHTML == "Refresh") {
+        document.location.reload()
+    }
+
 }
 
 
 function Refresh(){
-if (document.getElementById("changeMe").innerHTML = "géchangé" && document.getElementById('changeMe').clicked == true) {
+if (document.getElementById("changeMe").innerHTML == "Refresh" && document.getElementById('changeMe').clicked == true) {
     document.location.reload();
 }}

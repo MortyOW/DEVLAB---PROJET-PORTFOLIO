181dbcb7600b75b28b44e8e885a9ab305618adc1
MortyOW <adam.simon@edu.devinci.fr>
2022-11-04 00:04:16 +0100

    :sparkles: NavBar et Menu Burger

---
+++ onepage.js
@@ -0,0 +1,13 @@
+const hamburger = document.querySelector('.header .navbar .navlist .hamburger');
+const mobile_menu = document.querySelector('.header .navbar .navlist ul');
+const menu_item = document.querySelectorAll('.header .navbar .navlist ul li a');
+const header = document.querySelector('.header.container');
+
+
+
+if(hamburger){
+hamburger.addEventListener('click', () => {
+	hamburger.classList.toggle('active');
+	mobile_menu.classList.toggle('active');
+})};
+

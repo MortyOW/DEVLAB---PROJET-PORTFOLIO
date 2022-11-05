852760b14af65dd4a9dd46873a3ec1f79b7fff42
MortyOW <adam.simon@edu.devinci.fr>
2022-11-05 17:44:08 +0100

    :sparkles: feat footer

--- onepage.js
+++ onepage.js
@@ -1,5 +1,5 @@
 const hamburger = document.querySelector('.header .navbar .navlist .hamburger');
-const mobile_menu = document.querySelector('.header .navbar .navlist ul');
+const menu_mobile = document.querySelector('.header .navbar .navlist ul');
 const menu_item = document.querySelectorAll('.header .navbar .navlist ul li a');
 const header = document.querySelector('.header.container');
 
@@ -8,13 +8,13 @@ const header = document.querySelector('.header.container');
 if(hamburger){
 hamburger.addEventListener('click', () => {
 	hamburger.classList.toggle('active');
-	mobile_menu.classList.toggle('active');
+	menu_mobile.classList.toggle('active');
 })};
 
 menu_item.forEach((item) => {
 	item.addEventListener('click', () => {
 		hamburger.classList.toggle('active');
-		mobile_menu.classList.toggle('active');
+		menu_mobile.classList.toggle('active');
 	});
 });
 
